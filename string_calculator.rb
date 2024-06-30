7485a28006374cdafff22621ce6859b653700c09
Aman Jain <jainaman224@gmail.com>
2024-06-30 17:50:00 +0530

    Refactor

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,14 +2,28 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
+    delimiter, numbers = get_delimiter(numbers)
+    values = apply_delimiter(numbers, delimiter)
+    sum(values)
+  end
+
+  private
+
+  def get_delimiter(numbers)
     if numbers.start_with?("//")
       delimiter = numbers[2]
       numbers = numbers[4..-1]
-      values = numbers.split(delimiter)
     else
-      values = numbers.split(/\n|,/)
+      delimiter = /\n|,/
     end
+    [delimiter, numbers]
+  end
 
+  def apply_delimiter(numbers, delimiter)
+    numbers.split(delimiter)
+  end
+
+  def sum(values)
     values.map(&:to_i).sum
   end
 end
