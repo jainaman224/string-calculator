ddb5e77089ab2b8743bcd2026209be5f15c1c9f5
Aman Jain <jainaman224@gmail.com>
2024-06-30 15:40:51 +0530

    Handle test case for two comma separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,9 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.to_i
+    values = numbers.split(',')
+    return numbers.to_i if values.size == 1
+
+    values.map(&:to_i).sum
   end
 end
