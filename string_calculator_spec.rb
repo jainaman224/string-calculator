2c62babf79f784eba8252b265ca0d4a529a69416
Aman Jain <jainaman224@gmail.com>
2024-06-30 15:17:04 +0530

    Create test case for one number

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe StringCalculator do
         expect(string_calculator.add("")).to eq(0)
       end
     end
+
+    context 'when string has one number' do
+      it 'returns number as integer' do
+        string_calculator = StringCalculator.new
+        expect(string_calculator.add("1")).to eq(1)
+      end
+    end
   end
 end
